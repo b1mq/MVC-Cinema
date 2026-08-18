43e769ec5169ba7f642b4480d9419f31ba31c407
Egor <arnoldtagirov1@gmail.com>
2026-08-18 19:17:01 +0200

    добавленна коснтрукция из прошлого проекта

---
+++ WebApplication1/Services/FinderService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApplication1.Models;
+namespace WebApplication1.Services
+{
+    public class FinderService:IFinderService
+    {
+        public string Api = "https://www.omdbapi.com/?i=tt3896198&apikey=5c15d396";
+        private readonly HttpClient _httpClient;
+        public FinderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<Film> GetFinderAsync(string movie)
+        {
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var url = $"{Api}&t={Uri.EscapeDataString(movie)}";
+            var response = await _httpClient.GetFromJsonAsync<Film>(url,options);
+            if(response != null)
+            {
+                return response;
+            }else
+            {
+                return new Film {Title = "Not found" };
+            }
+            
+
+        }
+    }
+}
---
+++ WebApplication1/Services/IFinderService.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Services
+{
+    using WebApplication1.Models;
+    public interface IFinderService
+    {
+        public Task<Film> GetFinderAsync(string name);
+    }
+}
