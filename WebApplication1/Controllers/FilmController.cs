61dcfdf08b4d20fe794f7c5d6559b4e97b1138bd
Egor <damirtagirovwq@gmail.com>
2026-08-24 17:49:01 +0200

    Update FilmController.cs

--- WebApplication1/Controllers/FilmController.cs
+++ WebApplication1/Controllers/FilmController.cs
@@ -1,21 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
 
 namespace WebApplication1.Controllers
 {
+    /// <summary>
+    /// Контроллер для управления фильмами (Film).
+    /// Обеспечивает выполнение базовых CRUD-операций: просмотр списка, редактирование, удаление и просмотр деталей фильма.
+    /// </summary>
     public class FilmController : Controller
     {
         private readonly FilmContext _db;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="FilmController"/>.
+        /// </summary>
+        /// <param name="db">Контекст базы данных Entity Framework для работы с таблицей фильмов.</param>
         public FilmController(FilmContext db)
         {
             _db = db;
         }
 
+        /// <summary>
+        /// Асинхронно получает список всех фильмов и отображает главную страницу.
+        /// </summary>
+        /// <returns>Представление (View) с коллекцией всех фильмов.</returns>
         public async Task<IActionResult> Index()
         {
             IEnumerable<Film> films = await Task.Run(() => _db.films);
             return View(films);
         }
+
+        /// <summary>
+        /// Удаляет фильм из базы данных по его идентификатору.
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор удаляемого фильма.</param>
+        /// <returns>Перенаправление на главную страницу (метод <see cref="Index"/>).</returns>
         public async Task<IActionResult> Delete(int id)
         {
             var filmToRemove = await _db.films.FindAsync(id);
@@ -24,10 +46,15 @@ namespace WebApplication1.Controllers
             {
                 _db.films.Remove(filmToRemove);
                 await _db.SaveChangesAsync();
-
             }
             return RedirectToAction("Index");
         }
+
+        /// <summary>
+        /// Отображает форму для редактирования существующего фильма.
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор фильма, который нужно отредактировать.</param>
+        /// <returns>Представление с формой редактирования, если фильм найден; иначе возвращает ошибку 404 (NotFound).</returns>
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
@@ -38,6 +65,12 @@ namespace WebApplication1.Controllers
             } 
             return View("~/Views/Home/Index.cshtml",film);
         }
+
+        /// <summary>
+        /// Обрабатывает отправленную форму и сохраняет обновленные данные фильма в базу данных.
+        /// </summary>
+        /// <param name="film">Модель фильма с обновленными данными, полученными из формы.</param>
+        /// <returns>Перенаправление на главную страницу (метод <see cref="Index"/>) после успешного сохранения.</returns>
         [HttpPost]
         public async Task<IActionResult> Edit(Film film)
         {
@@ -45,6 +78,12 @@ namespace WebApplication1.Controllers
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        /// <summary>
+        /// Отображает страницу с детальной информацией о конкретном фильме.
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор фильма для просмотра.</param>
+        /// <returns>Представление 'MoviePage' с деталями фильма, если он найден; иначе возвращает ошибку 404 (NotFound).</returns>
         [HttpGet]
         public async Task<IActionResult> More(int id)
         {
@@ -55,7 +94,5 @@ namespace WebApplication1.Controllers
             }
             return View("MoviePage",film);
         }
-
-      
     }
 }
